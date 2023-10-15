923d402c77716e4b6ec56741acae68940cfd41d7
Dishant Sharma <dishu5570@gmail.com>
2023-10-15 11:09:43 +0530

    modified package.json

--- src/contants.ts
+++ src/contants.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BACKEND_URL = 'http://localhost:4000/api';
-// const BACKEND_URL = 'https://api.lazyweb.rocks/api';
+// const BACKEND_URL = 'http://localhost:4000/api';
+const BACKEND_URL = 'https://api.lazyweb.rocks/api';
 
 export const APIClient = axios.create({
   baseURL: BACKEND_URL,
